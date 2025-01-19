63d500da9ee6c320b2daf0dc82bb5a25c528ec4d
Joshua Niam <joshuaniamjy@gmail.com>
2025-01-19 18:31:58 +0800

    Added verifying sufficient resources function and report function

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -3,14 +3,30 @@ resources = {
     "water": 500,  # in ml
     "milk": 300,   # in ml
     "coffee": 100, # in grams
+    "money" : 0
 }
 
 # Needed resources and prices of coffee types
 menu = {
-    "espresso": {"water": 50, "milk": 0, "coffee": 18, "cost": 1.5},
-    "latte": {"water": 200, "milk": 150, "coffee": 24, "cost": 2.5},
-    "cappuccino": {"water": 250, "milk": 100, "coffee": 24, "cost": 3.0},
+    "espresso": {"water": 50, "milk": 0, "coffee": 18, "money": 1.5},
+    "latte": {"water": 200, "milk": 150, "coffee": 24, "money": 2.5},
+    "cappuccino": {"water": 250, "milk": 100, "coffee": 24, "money": 3.0},
 }
+
+def checkresources(order):
+    global resources
+    truefalse = False
+    """check the resources if enough for each order"""
+    for resource, value in menu[order].items():
+        if value >= resources[resource]:
+            truefalse = True
+        else:
+            return False
+
+    if truefalse:
+        return True
+
+
 
 # a loop while the coffee machine is on
 #take the order
@@ -25,10 +41,23 @@ while coffee_machine == "on":
         break
 # report
     elif order == 'report':
-        pass
+        print(*list(f"{item} = {value} " for item, value in resources.items()), sep="\n")
 
 #get a drink
     elif order in menu:
-        pass
+        if order == "espresso":
+            sufficient = checkresources("espresso")
+        elif order == "latte":
+            sufficient = checkresources("latte")
+        elif order == "cappuccino":
+            sufficient = checkresources("cappuccino")
+
+        if sufficient:
+            print("Sure!")
+        else:
+            print("Not enough resources.")
+
+
+
 
 
